be8cf3aae1052bdfc291121580110c3eef4ac4b4
Y.H.JEONG <fx13577@gmail.com>
2022-04-17 18:41:41 +0900

    add article

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {Card, Button, Container, Row, Col} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [articles, setArticles] = useState([])
+    const getData = async () => {
+
+        try{
+            const {data} = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2022-03-17&sortBy=publishedAt&apiKey=f1c87e2bb86248de9f9492e513f93e1f")
+            setArticles(data.articles)
+            console.log(articles)
+        }catch (e){
+            console.log(e)
+        }
+    }
+    useEffect(()=>{
+        getData()
+    }, [])
+
+
+    return (
+      <Container>
+          <Row>
+              {articles.map((article, index) => (
+                  <Col key={index}>
+                  <Card style={{ width: '18rem' }}>
+                      <Card.Img variant="top" src={article.urlToImage ? article.urlToImage : require("./assets/empty_image.png")} />
+                      <Card.Body>
+                          <Card.Title>C{article.title.slice(0,50)}</Card.Title>
+                          <Card.Text>
+                              {article.description.slice(0,100)}
+                          </Card.Text>
+                          <Button variant="primary">Go somewhere</Button>
+                      </Card.Body>
+                  </Card>
+                  </Col>
+              ))}
+          </Row>
+
+      </Container>
+
+    );
+};
 
 export default App;
